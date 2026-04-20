10546d3b14cce41ffa522fd5b1ddd05fe991137e
errorayu498-ship-it <errorayu498@gmail.com>
2026-04-20 23:32:51 +0500

    Add files via upload

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,1 +1,22 @@
+const { logger } = require('../utils/logger');
+const { GiveawayManager } = require('../utils/giveawayManager');
 
+module.exports = {
+    name: 'ready',
+    once: true,
+    async execute(client) {
+        logger.success(`${client.user.tag} is online and ready!`);
+        
+        // Load active giveaways
+        await GiveawayManager.loadGiveaways(client);
+        
+        // Cache invites
+        const invites = await client.guilds.cache.get(process.env.GUILD_ID)?.invites.fetch();
+        if (invites) {
+            client.invites.set(process.env.GUILD_ID, new Map(invites.map(invite => [invite.code, invite.uses])));
+        }
+        
+        logger.info(`Loaded ${client.giveaways.size} active giveaways`);
+        logger.info(`Loaded invite cache for ${client.invites.size} guilds`);
+    }
+};
